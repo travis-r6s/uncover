92189e29953a95565e26f65679f92f1284703abe
Travis Reynolds <travis@cirruscreative.co.uk>
2019-09-05 00:23:48 +0100

    add browser name

--- uncover/nuxt.config.js
+++ uncover/nuxt.config.js
@@ -14,11 +14,11 @@ export default {
   },
 
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Uncover',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'An Instagram-style app showing off Remote Joins' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }

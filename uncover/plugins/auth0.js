b6e35b5d67d3580cad6fc12d2ef191b401b8dd0e
Travis Reynolds <travis@cirruscreative.co.uk>
2019-09-05 00:21:55 +0100

    Add getUser method

--- uncover/plugins/auth0.js
+++ uncover/plugins/auth0.js
@@ -1,4 +1,5 @@
 import auth0Js from 'auth0-js'
+import decode from 'jwt-decode'
 
 const APP_URL = process.env.APP_URL
 const CLIENT_DOMAIN = process.env.AUTH0_CLIENT_DOMAIN
@@ -13,17 +14,35 @@ const webAuth = new auth0Js.WebAuth({
   scope: 'openid profile email'
 })
 
-const authFunctions = {
-  login: () => webAuth.authorize(),
-  parseHash: hash => new Promise((resolve, reject) => {
+const authFunctions = ctx => {
+  const login = () => webAuth.authorize()
+  const parseHash = hash => new Promise((resolve, reject) => {
     webAuth.parseHash({ hash }, (err, authResult) => {
       if (err) reject(err)
       resolve(authResult)
     })
-  }),
-  logout: () => webAuth.logout({ returnTo: `${APP_URL}`, clientID: CLIENT_ID })
+  })
+  const getUser = ({ app }) => {
+    const token = app.$apolloHelpers.getToken()
+    if (!token) return {}
+    const user = decode(token)
+    const hasura = user[ 'https://hasura.io/jwt/claims' ]
+    return {
+      id: hasura[ 'x-hasura-user-id' ],
+      email: hasura[ 'x-hasura-user-email' ]
+    }
+  }
+
+  const logout = () => webAuth.logout({ returnTo: `${APP_URL}`, clientID: CLIENT_ID })
+
+  return {
+    login,
+    parseHash,
+    logout,
+    user: getUser(ctx)
+  }
 }
 
 export default (ctx, inject) => {
-  inject('auth', authFunctions)
+  inject('auth', authFunctions(ctx))
 }

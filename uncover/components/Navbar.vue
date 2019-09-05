cfa3365e61a44a667d97652feeae2f56be1a370a
Travis Reynolds <travis@cirruscreative.co.uk>
2019-09-05 15:26:37 +0100

    add logout method

--- uncover/components/Navbar.vue
+++ uncover/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar>
+  <b-navbar fixed-top>
     <template slot="brand">
       <b-navbar-item
         :to="{ path: '/' }"
@@ -29,10 +29,24 @@
         <b-icon icon="image-filter" />&nbsp;
         <span>New Post</span>
       </b-navbar-item>
-      <b-navbar-item href="#">
-        <b-icon icon="account-outline" />&nbsp;
-        <span>Account</span>
-      </b-navbar-item>
+      <b-navbar-dropdown
+        hoverable>
+        <template slot="label">
+          <b-icon icon="account-outline" />&nbsp;
+          <span>Account</span>
+        </template>
+        <b-navbar-item href="#">
+          About
+        </b-navbar-item>
+        <hr>
+        <b-navbar-item tag="div">
+          <b-button
+            @click="logout"
+            type="is-white">
+            Sign Out
+          </b-button>
+        </b-navbar-item>
+      </b-navbar-dropdown>
     </template>
 
     <template
@@ -58,6 +72,12 @@
 export default {
   computed: {
     isAuthenticated () { return !!this.$apolloHelpers.getToken() }
+  },
+  methods: {
+    async logout () {
+      await this.$$apolloHelpers.onLogout()
+      this.$auth.logout()
+    }
   }
 }
 </script>

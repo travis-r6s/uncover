468966dc5b1ce01ab53dcd7546e33572a0ead8ef
Travis Reynolds <travis@cirruscreative.co.uk>
2019-09-13 13:19:04 +0100

    create function to query remote joins

--- functions/TimerTrigger/index.js
+++ functions/TimerTrigger/index.js
@@ -1,9 +1,13 @@
+const { request } = require('graphql-request')
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.IsPastDue)
-    {
-        context.log('JavaScript is running late!');
+  var timeStamp = new Date().toISOString()
+
+  const query = `{
+    images(limit: 10) {
+      id
+      transform_url
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
-};
+  }`
+  await request('https://uncover-app.herokuapp.com', query)
+  context.log('JavaScript timer trigger function ran!', timeStamp)
+}
